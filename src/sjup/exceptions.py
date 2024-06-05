fa784666dfa5c27416cbbae3d074774c8b20c5f9
Kasper Munch <kaspermunch@birc.au.dk>
2024-06-05 20:13:53 +0200

    Update

--- src/sjup/exceptions.py
+++ src/sjup/exceptions.py
@@ -1,22 +1,3 @@
-from gwf.exceptions import GWFError
 
-
-class BackendError(GWFError):
+class StopServerException(Exception):
     pass
-
-
-class UnsupportedOperationError(BackendError):
-    pass
-
-
-class DependencyError(BackendError):
-    pass
-
-
-class TargetError(BackendError):
-    pass
-
-
-class LogError(BackendError):
-    def __init__(self):
-        super().__init__("Log not found.")
